2c65b24e798f6c8163448d3b2a475554d886535d
Jet-Green <grishadzyin@gmail.com>
2026-01-02 20:08:53 +0500

    add throttle

--- app/composables/useBookForm.ts
+++ app/composables/useBookForm.ts
@@ -2,11 +2,18 @@ import { toast } from "vue3-toastify";
 import BookFormApi from "~/api/BookFormApi";
 
 export default function useBookForm() {
+  let submitCount = useState<number>(() => 0)
+
   return {
     // functions
     async sendForm(name: string, phone: string, date: string, eventType?: string) {
+      if (submitCount.value > 2) {
+        toast.error("Слишком много отправленных форм!")
+        return
+      }
       try {
         await BookFormApi.sendForm(name, phone, date, eventType)
+        submitCount.value += 1;
 
         toast("Заявка отправлена!", { type: "success", autoClose: 2200 })
       } catch (error) {
