b7943861f9ebd91acd30eb59853460e1438a0b3e
Jet-Green <grishadzyin@gmail.com>
2026-01-20 22:02:43 +0500

    add meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,21 +12,50 @@ export default defineNuxtConfig({
       viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0',
       title: "Облака - ресторан-бар",
       meta: [
+        { name: 'yandex-verification', content: 'b2615989a7977ad7' },
         {
-          name: 'description', content: `Самый высокий бар-ресторан города с летней террасой Мы предлагаем вам обширную карту коктейльной классики, изысканные блюда средиземноморской, паназиатской и европейской кухни.`
+          name: 'description',
+          content: `Самый высокий бар-ресторан города с летней террасой Мы предлагаем вам обширную карту коктейльной классики, изысканные блюда средиземноморской, паназиатской и европейской кухни.`
         },
         { name: 'keywords', content: 'пермь,облака,бар-ресторан,бар,эскобар,room bar,рум бар' },
-        { property: 'icon', content: "https://oblaka-perm.ru/favicon.ico" },
-        // Open Graph (для соцсетей)
+        { name: 'icon', content: "https://oblaka-perm.ru/favicon.ico" }, // ← исправлено: name, а не property
+        // Open Graph
         { property: 'og:title', content: 'Облака - ресторан-бар' },
         { property: 'og:description', content: 'Самый высокий бар-ресторан города с летней террасой Мы предлагаем вам обширную карту коктейльной классики, изысканные блюда средиземноморской, паназиатской и европейской кухни' },
         { property: 'og:image', content: '/icons/logo.png' },
         { property: 'og:type', content: 'website' },
         { property: 'og:url', content: 'https://oblaka-perm.ru/' },
-        { property: "twitterTitle", content: 'Облака - ресторан-бар' },
-        { property: "twitterDescription", content: 'Самый высокий бар-ресторан города с летней террасой Мы предлагаем вам обширную карту коктейльной классики, изысканные блюда средиземноморской, паназиатской и европейской кухни' },
-        { property: "twitterImage", content: 'https://oblaka-perm.ru/favicon.ico' },
-        { property: "twitterCard", content: 'summary' },
+        // Twitter (исправлены названия — должны быть с префиксом `twitter:`)
+        { name: 'twitter:title', content: 'Облака - ресторан-бар' },
+        { name: 'twitter:description', content: 'Самый высокий бар-ресторан города с летней террасой Мы предлагаем вам обширную карту коктейльной классики, изысканные блюда средиземноморской, паназиатской и европейской кухни' },
+        { name: 'twitter:image', content: 'https://oblaka-perm.ru/favicon.ico' },
+        { name: 'twitter:card', content: 'summary' },
+      ],
+      script: [
+        {
+          innerHTML: `
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=106354730', 'ym');
+          ym(106354730, 'init', {
+            ssr: true,
+            webvisor: true,
+            clickmap: true,
+            ecommerce: "dataLayer",
+            accurateTrackBounce: true,
+            trackLinks: true
+          });
+        `,
+          type: 'text/javascript',
+        },
+      ],
+      noscript: [
+        {
+          innerHTML: '<div><img src="https://mc.yandex.ru/watch/106354730" style="position:absolute; left:-9999px;" alt="" /></div>',
+        },
       ],
     },
   },
@@ -47,13 +76,13 @@ export default defineNuxtConfig({
     groups: [
       {
         userAgent: '*',
-        disallow: ['/admin/**','/login/**','/registration/**'],
+        disallow: ['/admin/**', '/login/**', '/registration/**'],
       },
     ]
   },
 
   sitemap: {
-    exclude: ['/admin/**','/login/**','/registration/**'],
+    exclude: ['/admin/**', '/login/**', '/registration/**'],
     defaults: {
       changefreq: 'weekly',
       priority: 0.8,
